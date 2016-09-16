e3f4887160ac1dcd15098b5ff2a2d23157fd9c6b
Drayden Lin <drayden.lin@gmail.com>
2016-09-16 15:52:51 +0800

    Initial Commit
    Starting at tutorial 6

--- SDL_Sample/Main.cpp
+++ SDL_Sample/Main.cpp
@@ -3,8 +3,8 @@
 #include <string>
 #include <SDL_image.h>
 
-const int SCREEN_WIDTH  = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH  = 800;
+const int SCREEN_HEIGHT = 600;
 
 bool init();
 bool loadMedia();
